338e892025c18414808cd6e0bc45e66b4a122bb4
Ariel Shum <ariegg210@gmail.com>
2020-11-22 17:00:32 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Monocular Depth Estimation with an absolute rangefinder point for absolute depth',
+    author='Ariel Shumacher',
+    license='',
+)
