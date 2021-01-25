visualizations for samples, normalization by std mean and minmax. optionally remove sigmoid in tiny_unet and fixed the fucking nans.

src/data_loader.py
@@ -23,7 +23,7 @@ cfg_aug = defs.cfg['data_augmentation']
 class GeoposeDataset(Dataset):
     """GeoPose3K dataset with (img,depth) pairs."""
 
-    def __init__(self, transform=None):
+    def __init__(self, transform=None, onlydepth=False):
         """
 
         Args:
@@ -35,44 +35,54 @@ class GeoposeDataset(Dataset):
         self.foldernames = np.array(
             [fn for fn in os.listdir(self.dir) if os.path.isdir(join(self.dir, fn)) and not fn.startswith('.')])
         self.foldernames.sort()
+        self.onlydepth = onlydepth
 
     def __len__(self):
         return len(self.foldernames)
 
     def __getitem__(self, idx):
+        """
+        get sample item
+        Args:
+            idx: int or string. if int: will get by idx in foldernames list,
+                                if string: will get sample by folder name supplied in string.
+
+        Returns:
+
+        """
         # batch requests
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        folder = os.path.join(self.dir, self.foldernames[idx])
-        # folder = os.path.join(self.dir, 'flickr_sge_987018572_c163ca22e5_1146_30222664@N00')
-        img_names = []
-        depth_names = []
-        segmap_names = []
-        loader = PFMLoader()
 
-        # for folder in batch_folders:
-        #     print(folder)
+        if type(idx) == str:
+            # assume idx is folder name requested. for visualization purposes mostly.
+            folder = os.path.join(self.dir, idx)
+        else:
+            folder = os.path.join(self.dir, self.foldernames[idx])
+        depth_name = None
+        segmap_name = None
+        img_name = None
+        loader = PFMLoader()
         for file in os.listdir(folder):
             if file == 'distance_crop.pfm':
                 depth_name = os.path.join(folder, file)
-                # depth_names.append(os.path.join(folder, file))
-            if file.endswith(('.png', '.jpg', '.jpeg')):
+            elif file.endswith(('.png', '.jpg', '.jpeg')):
                 if file == 'labels_crop.png':
-                    # segmap_names.append(join(folder, file))
                     segmap_name = join(folder, file)
-                else:
-                    # img_names.append(os.path.join(folder, file))
+                elif file.startswith('photo'):
                     img_name = os.path.join(folder, file)
-        if len(img_names) != len(depth_names) or len(segmap_names) != len(depth_names):
-            raise Exception(f'don\'t have same amount of images/segmaps as depths.\n'
-                            # f' imgs: {len(img_names)}\ndepths: {len(depth_names)}\n'
-                            f'folder: {folder}')
+        if not depth_name or not segmap_name or not img_name:
+            raise Exception(f'folder is missing image/depth/seg: {folder}')
+
+        if self.onlydepth:
+            return {'depth': np.array(loader.load_pfm(depth_name)[::-1])}
         # image = Image.open(img_name)
-        image = io.imread(img_name)
+        image = np.array(io.imread(img_name))
         depth = np.array(loader.load_pfm(depth_name)[::-1])
-        segmap = io.imread(segmap_name)[:, :, :3]  # alpha channel is irrelevant (val is always 1).
+        # segmap = io.imread(segmap_name)[:, :, :3]  # alpha channel is irrelevant (val is always 1).
         # bad - all images are in different resolutions!!!
-        sample = {'image': image, 'segmap': segmap, 'depth': depth, 'name': self.foldernames[idx]}
+        name = idx if type(idx) == str else self.foldernames[idx]
+        sample = {'image': image, 'depth': depth, 'name': name}
 
         if self.transform:
             sample = self.transform(sample)
@@ -80,7 +90,7 @@ class GeoposeDataset(Dataset):
         return sample
 
 
-class GeoposeToTensor():
+class GeoposeToTensor:
     def __call__(self, sample):
         for k, v in sample.items():
             if type(v).__name__ == 'ndarray':

src/other_models/tiny_unet.py
@@ -64,11 +64,13 @@ class UNet(nn.Module):
     Used as a toy net to see that it works at all.
     """
 
-    def __init__(self, tiny=True):
+    def __init__(self, tiny=True, sigmoid=True):
         super().__init__()
-        mult = 4
         if tiny:
             mult = 1
+        else:
+            mult = 4
+        self.sigmoid = sigmoid
         self.activation = F.relu
 
         self.pool1 = nn.MaxPool2d(2)
@@ -105,5 +107,7 @@ class UNet(nn.Module):
         up3 = self.up_block32_16(up2, block1)
 
         last = self.last(up3)
-
-        return torch.sigmoid(last.squeeze())
+        if self.sigmoid:
+            return torch.sigmoid(last.squeeze())
+        else:
+            return last.squeeze()

src/prepare_data.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
-# from pypfm import PFMLoader
+from pypfm import PFMLoader
 from skimage import io
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.transforms import functional as TF
 from PIL import ImageOps
@@ -21,8 +21,9 @@ from PIL import Image
 import os
 from os.path import join
 from matplotlib import pyplot as plt
-from visualize import blend_images
 from data_loader import GeoposeDataset, GeoposeToTensor
+from skimage.transform import resize
+import cv2
 
 
 class ResizeToImgShape:
@@ -35,7 +36,7 @@ class ResizeToImgShape:
         Args:
             sample: dict(Tensors), data sample with an 'image', 'depth' and possibly other images
         """
-        _, h, w = sample['image'].shape
+        c, h, w = sample['image'].shape
         for k, img in sample.items():
             if k == 'image':
                 continue
@@ -44,9 +45,11 @@ class ResizeToImgShape:
                 # and it's worse to have -.8 than some out of place -1's (for sky values).
                 # segmap really can't be interpolated, bc values are supposed to be constant.
                 img = TF.resize(img, (h, w), interpolation=Image.NEAREST)
-                # viz.tensor_imshow(img)
                 sample[k] = img
-
+                # viz.tensor_imshow(img)
+            elif type(img) == np.ndarray:
+                img = resize(img, (c, h))
+                sample[k] = img
         # blended = blend_images(TF.to_pil_image(image), TF.to_pil_image(depth_resized))
         # plt.imshow(blended)
         # plt.show()
@@ -175,20 +178,67 @@ class ExtractSegmentationMask:
     # TODO: next week, the whole segmentation git.
 
 
+class NormalizeDepth:
+    def __call__(self, sample):
+        depth_mean = torch.Tensor([3808.661])
+        depth_std = torch.Tensor([3033.6562])
+        # adding min to norm so all values will be above 0 (since we're using rmsle).
+        sample['depth'] = TF.normalize(sample['depth'], depth_mean, depth_std) + torch.Tensor(
+            depth_mean - 1 / depth_std).to(device=defs.get_dev())
+        return sample
+
+class NormalizeImg:
+    """normalize images and depths based on our crop results."""
+
+    # {'image': tensor([0.4168, 0.4701, 0.5008]), 'depth': array([3808.661], dtype=float32)}
+    # {'image': tensor([0.1869, 0.1803, 0.1935]), 'depth': array([3033.6562], dtype=float32)}
+    def __call__(self, sample):
+        image_mean = torch.Tensor([0.4168, 0.4701, 0.5008])
+        image_std = torch.Tensor([0.1869, 0.1803, 0.1935])
+        sample['image'] = TF.normalize(sample['image'], image_mean, image_std)
+        return sample
+
+
+class NormMinMaxDepth:
+    def __call__(self, sample):
+        min = -1
+        max = 254957
+        depth = sample['depth']
+        depth -= 1
+        depth /= max
+        sample['depth'] = depth
+        return sample
+
+
 def crop_to_aspect_ratio_and_resize():
     cfg_ds = defs.cfg['dataset']
     aspect_ratio, h, w = cfg_ds['aspect_ratio'], cfg_ds['h'], cfg_ds['w']
-    return transforms.Compose([GeoposeToTensor(),
+    return transforms.Compose([FillNaNsFFS(),
+                               GeoposeToTensor(),
                                CropToAspectRatio(aspect_ratio=aspect_ratio),
                                ResizeToResolution(h, w),
-                               ExtractSkyMask()])
+                               NormalizeImg(),
+                               NormMinMaxDepth()])
+                               # ExtractSkyMask()])
 
 
 def pad_and_center():
-    return transforms.Compose([GeoposeToTensor(),
+    cfg_ds = defs.cfg['dataset']
+    h, w = cfg_ds['h'], cfg_ds['w']
+    return transforms.Compose([FillNaNsFFS(),
+                               GeoposeToTensor(),
                                ResizeToImgShape(),
                                PadToResolution(h, w),
-                               CenterAndCrop(h, w)])
+                               CenterAndCrop(h, w),
+                               NormalizeImg(),
+                               NormMinMaxDepth()])
+
+
+class FillNaNsFFS:
+    def __call__(self, sample):
+        depth = sample['depth']
+        sample['depth'] = np.nan_to_num(depth)
+        return sample
 
 
 if __name__ == '__main__':
@@ -206,10 +256,11 @@ if __name__ == '__main__':
     cfg_ds = defs.cfg['dataset']
     aspect_ratio, h, w = cfg_ds['aspect_ratio'], cfg_ds['h'], cfg_ds['w']
 
-    shitty_transform = transforms.Compose([GeoposeToTensor(),
+    shitty_transform = transforms.Compose([FillNaNsFFS(),
+                                           GeoposeToTensor(),
                                            ResizeToImgShape(),
                                            ResizeToResolution(h, w)])
-    geoset = GeoposeDataset(transform=crop_to_aspect_ratio_and_resize())
+    geoset = GeoposeDataset(transform=pad_and_center())
     dl = DataLoader(geoset, batch_size=4, shuffle=True)
     batch = next(iter(dl))
     # for k, v in s2.items():

src/train.py
@@ -10,12 +10,12 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader, Subset, random_split
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
-
+from matplotlib import pyplot as plt
 import model
 import visualize as viz
 from data_loader import FarsightDataset, GeoposeDataset, FarsightToTensor, get_farsight_fold_dataset
 from other_models.tiny_unet import UNet
-from prepare_data import crop_to_aspect_ratio_and_resize
+from prepare_data import crop_to_aspect_ratio_and_resize, pad_and_center
 from utils import get_dev, cfg, get_folder_name, set_cfg
 import utils
 
@@ -57,7 +57,9 @@ def train():
     epochs = cfg_train['epochs']
     print_every = cfg_train['print_every']
     save_every = cfg_checkpoint['save_every']
+    # use_writer = cfg['misc']['use_writer']
     folder_name = get_folder_name()
+    # if use_writer:
     writer = SummaryWriter(os.path.join('runs', folder_name))
     train_loader, val_loader = get_loaders()
     n_batches = len(train_loader)
@@ -85,6 +87,7 @@ def train():
                 img, gt_depth = data['image'], data['depth']
                 loss, pred_depth = step(criterion, img, gt_depth, net, optimizer)
                 loss_value = loss.item()
+                assert loss_value == loss_value, 'loss is nan! tf?'
                 pbar.set_postfix(**{'loss (batch)': loss_value})
                 running_loss += loss_value
                 pbar.update()
@@ -210,12 +213,12 @@ def print_stats(train_sample, val_sample,
     logger.info('logging images...')
     fig = viz.show_batch(train_sample)
     fig.suptitle(f'train, epoch {epoch}', fontsize='xx-large')
-    # plt.show()
+    plt.show()
     writer.add_figure(tag='viz/train', figure=fig, global_step=epoch)
     if val_sample is not None:
         fig = viz.show_batch(val_sample)
         fig.suptitle(f'val, epoch {epoch}', fontsize='xx-large')
-        # plt.show()
+        plt.show()
         writer.add_figure(tag='viz/val', figure=fig, global_step=epoch)
 
 
@@ -234,7 +237,11 @@ def get_net():
     cfg_optim = cfg['optim']
     model_name = cfg_model['name'].lower()
     if model_name == 'unet':
-        net = UNet()
+        if cfg['dataset']['name'] == 'geopose':
+            net = UNet(sigmoid=True)
+            # TODO: fix if you don't do minmax scaling in the end.
+        else:
+            net = UNet()
     elif model_name == 'toynet':
         net = model.toyNet()
     else:
@@ -261,6 +268,7 @@ def get_criterion():
         raise ValueError("can only use rmsle or mse")
     return criterion
 
+
 def get_loaders():
     ds_name = cfg['dataset']['name']
     if ds_name == 'farsight':
@@ -277,7 +285,7 @@ def get_geopose_loaders():
     batch_size_val = cfg['validation']['batch_size']
     val_percent = cfg_validation['val_percent']
     subset_size = cfg_train['subset_size']
-    ds = GeoposeDataset(transform=crop_to_aspect_ratio_and_resize())
+    ds = GeoposeDataset(transform=pad_and_center())
     if subset_size is not None:
         ds = Subset(ds, range(subset_size))
     n_val = int(len(ds) * val_percent)
@@ -294,7 +302,6 @@ def get_geopose_loaders():
                             batch_size=batch_size_val,
                             num_workers=0)
     return train_loader, val_loader
-
 
 
 def get_farsight_loaders():

src/visualize.py
@@ -4,6 +4,10 @@ import torch
 from mpl_toolkits.axes_grid1 import ImageGrid
 import numpy as np
 from PIL import Image
+import data_loader
+from torchvision.transforms import functional as TF
+from matplotlib import cm
+import matplotlib.colors as colors
 
 
 class Unravel:
@@ -47,13 +51,15 @@ def show_batch(batch):
         filenames = batch.pop('name')
     except:
         filenames = None
-    keys, values = list(batch.keys()), list(batch.values())  # images_batches: list(batches)
-    batch_size = len(values[0])
-    tuple_size = len(values)  # amount of images in per sample we'll be displaying.
+    image_type_names, image_types = list(batch.keys()), list(batch.values())  # image_type_names: image, depth, etc.
+    # for image_type in image_types:
+    #     if len(image_type.shape) == 2 or (len(image_type.shape) == 3 and
+    batch_size = image_types[0].shape[0]
+    tuple_size = len(image_types)  # amount of images in per sample we'll be displaying.
     u = Unravel(tuple_size, batch_size)
     fig, ax = plt.subplots(tuple_size, batch_size, figsize=(batch_size * 4 + 2, tuple_size * 4))
     # image type
-    for i, type_name in enumerate(keys):
+    for i, type_name in enumerate(image_type_names):
         ax[u(i * batch_size)].set_ylabel(type_name, fontsize='x-large')
     # sample number
     if filenames is not None:
@@ -65,36 +71,93 @@ def show_batch(batch):
         for j in range(batch_size):
             ax[u(j)].set_title('sample #{}'.format(j))
     # display actual image.
-    for i, img_batch in enumerate(values):
-        if torch.is_tensor(img_batch):
-            img_batch = img_batch.detach().cpu().numpy()
-            if img_batch.shape[1] == 3:
+    for i, image_type in enumerate(image_types):
+        if image_type_names[i] == 'depth':
+            norm = colors.LogNorm()
+        else:
+            norm = None
+        if torch.is_tensor(image_type):
+            image_type = image_type.detach().cpu().numpy()
+            if image_type.shape[1] == 3:
                 # reshape back from C X H X W into H X W X C.
-                img_batch = img_batch.transpose(0, 2, 3, 1)
-            elif len(img_batch.shape) == 4 and img_batch.shape[1] == 1:
+                image_type = image_type.transpose(0, 2, 3, 1)
+            elif len(image_type.shape) == 4 and image_type.shape[1] == 1:
                 # Grayscale image that wasn't yet squeezed.
-                img_batch = img_batch.squeeze(1)
-        for j, img in enumerate(img_batch):
-            ax[u(i * batch_size + j)].imshow(img)
+                image_type = image_type.squeeze(1)
+            elif len(image_type.shape) == 2:
+                image_type = image_type.unsqueeze(0)
+        for j, img in enumerate(image_type):
+            ax[u(i * batch_size + j)].imshow(img, norm=norm)
     return fig
 
 
+def show_sample(sample):
+    for k, v in sample.items():
+        if k == 'name':
+            sample[k] = [v]
+        else:
+            sample[k] = v[np.newaxis]
+    return show_batch(sample)
+
+
 def blend_images(im1, im2):
+    # im = Image.fromarray(np.uint8(cm.(im1) * 255))
+    if type(im1) == np.ndarray:
+        im1 = Image.fromarray(im1)
+    elif torch.is_tensor(im1):
+        im1 = TF.to_pil_image(im1)
+    if type(im2) == np.ndarray:
+        im2 = Image.fromarray(im2)
+    elif torch.is_tensor(im2):
+        im2 = TF.to_pil_image(im2)
     im1 = im1.convert("RGBA")
     im2 = im2.convert("RGBA")
-    blended = Image.blend(im1, im2, alpha=.5)
+    blended = Image.blend(im1, im2, alpha=.25)
     return blended
 
 
+def get_sub_batch(batch, subsize):
+    """
+    return first subsize samples in batch.
+    Args:
+        batch: batch of samples.
+        subsize:
+
+    Returns: sub-batch.
+
+    """
+    minibatch = {}
+    for k,v in batch.items():
+        minibatch[k] = v[:subsize]
+    return minibatch
 def tensor_imshow(img):
-    plt.imshow(img.cpu().numpy().transpose((1, 2, 0)))
-    plt.show()
+    """
+    plots tensor img on plt. doesn't plt.show it though.
+    Args:
+        img: Tensor of shape (CxHxW)
+
+    Returns: fig of image.
+
+    """
+    return plt.imshow(img.cpu().numpy().transpose((1, 2, 0)))
+
+
+def show_geopose_sample_with_blend(sample):
+    """
+    util for showing a specific sample in geopose dataset with blend.
+    for debugging purposes.
+    Returns: fig of plotted image.
+
+    """
+    sample['blend'] = blend_images(sample['image'], sample['depth'])
+    return show_sample(sample)
 
 
 if __name__ == '__main__':
-    torch.manual_seed(42)
-    n_samples = 1
-    x = torch.randn(n_samples * 100).view(n_samples, 10, 10)
-    y = torch.randn(n_samples * 100).view(n_samples, 10, 10)
-    show_batch({'x': x, 'y': y})
-    plt.show()
+    pass
+    # torch.manual_seed(42)
+    # n_samples = 1
+    # x = torch.randn(n_samples * 100).view(n_samples, 10, 10)
+    # y = torch.randn(n_samples * 100).view(n_samples, 10, 10)
+    # show_batch({'x': x, 'y': y})
+    # plt.show()